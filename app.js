0f67ecf3d8562845853aab865421731116a7c87d
MargoT <mtereshc@gmail.com>
2016-11-14 21:02:01 -0500

    first commit

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
-//
-// hello-mongoose: MongoDB with Mongoose on Node.js example on Heroku.
-// Mongoose is a object/data mapping utility for the MongoDB database.
-//
-
-// by Ben Wen with thanks to Aaron Heckmann
-
-//
-// Copyright 2015 ObjectLabs Corp.  
-// ObjectLabs operates MongoLab.com a MongoDb-as-a-Service offering
-//
-// MIT Licensed
-//
-
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation files
-// (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of the Software,
-// and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:  
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software. 
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE. 
-
+var SERVER_NAME = 'patient-api'
 //
 // Preamble
 var http = require ('http');	     // For serving a basic web page.
 var mongoose = require ("mongoose"); // The reason for this demo.
 
 // Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.  
-var uristring = 
-  process.env.MONGODB_URI || 
+// localhost if we don't find one.
+var uristring =
+  process.env.MONGODB_URI ||
   'mongodb://localhost/HelloMongoose';
 
 // The http server will listen to an appropriate port, or default to
@@ -49,118 +16,244 @@ var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
-  if (err) { 
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-    console.log ('Succeeded connected to: ' + uristring);
-  }
+var patientSchema = new mongoose.Schema({
+    firstName: String,
+		lastName: String,
+    gender: String,
+    cellPhoneNumber: String,
+    healthInsuranceNumber: String,
+		address: String,
+		dateOfBirth: String,
+		department: String,
+		doctor: String
 });
 
-// This is the schema.  Note the types, validation and trim
-// statements.  They enforce useful constraints on the data.
-var userSchema = new mongoose.Schema({
-  name: {
-    first: String,
-    last: { type: String, trim: true }
-  },
-  age: { type: Number, min: 0}
+var recordSchema = new mongoose.Schema({
+		date: String,
+		doctorId: String,
+		tempreture: String,
+		bloodPressure: String,
+		respiration: String,
+		heartBeat: String,
+    bloodOxygen: String,
+    allergies: String,
+    nausea: String,
+    appetite: String,
+    painArea: String,
+    painLevel: String,
+    medications: String,
+    visitReason: String,
+    healthBackground: String,
+    emergencyLevel: String,
 });
 
 // Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
-var PUser = mongoose.model('PowerUsers', userSchema);
+// nonexistent) the 'Patients' collection in the MongoDB database
+var Patient = mongoose.model('Patient', patientSchema);
+var Record = mongoose.model('Record', recordSchema);
 
-// Clear out old data
-PUser.remove({}, function(err) {
-  if (err) {
-    console.log ('error deleting old data.');
-  }
-});
+var restify = require('restify')
+  // Create the restify server
+  , server = restify.createServer({ name: SERVER_NAME})
 
-// Creating one user.
-var johndoe = new PUser ({
-  name: { first: 'John', last: 'Doe' },
-  age: 25
-});
+	if (typeof ipaddress === "undefined") {
+		//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+		//  allows us to run/test the app locally.
+		console.warn('No process.env.IP var, using default: ' + DEFAULT_HOST);
+		ipaddress = DEFAULT_HOST;
+	};
 
-// Saving it to the database.  
-johndoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var janedoe = new PUser ({
-  name: { first: 'Jane', last: 'Doe' },
-  age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var alicesmith = new PUser ({
-  name: { first: 'Alice', last: 'Smith' },
-  age: 45
-});
-alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
+	if (typeof port === "undefined") {
+		console.warn('No process.env.PORT var, using default port: ' + DEFAULT_PORT);
+		port = DEFAULT_PORT;
+	};
 
 
-// In case the browser connects before the database is connected, the
-// user will see this message.
-var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+  server.listen(port, ipaddress, function () {
+  console.log('Server %s listening at %s', server.name, server.url)
+  console.log('Endpoints:');
 
-// Create a rudimentary http server.  (Note, a real web application
-// would use a complete web framework and router like express.js). 
-// This is effectively the main interaction loop for the application. 
-// As new http requests arrive, the callback function gets invoked.
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  createWebpage(req, res);
-}).listen(theport);
+  console.log('%s/patients method: POST', server.url);
+  console.log('%s/patients method: GET', server.url);
+  console.log('%s/patients/:id method: GET', server.url);
+  console.log('%s/patients/hi/:healthInsuranceNumber method: GET', server.url);
 
-function createWebpage (req, res) {
-  // Let's find all the documents
-  PUser.find({}).exec(function(err, result) { 
-    if (!err) { 
-      res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
-      // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
-      var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
-      query.where('age').gt(64);
-      query.exec(function(err, result) {
-	if (!err) {
-	  res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
-	} else {
-	  res.end('Error in second query. ' + err)
-	}
+  console.log('%s/patients/:id/records method: POST', server.url);
+  console.log('%s/patients/:id/records method: GET', server.url);
+  console.log('%s/patients/:id/records/:id method: GET', server.url);
+
+
+
+})
+
+
+  server
+    // Allow the use of POST
+    .use(restify.fullResponse())
+
+    // Maps req.body to req.params so there is no switching between them
+    .use(restify.bodyParser())
+
+    //POST
+    // Create a new patient
+    server.post('/patients', function (req, res, next) {
+      console.log("**                                   **");
+      console.log('Processed Request --> POST request: patients');
+      // Make sure name is defined
+      if (req.params.firstName === undefined) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('first_name must be supplied'))
+      }
+      if (req.params.lastName === undefined) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('last_name must be supplied'))
+      }
+
+      // Creating new patient.
+      var newPatient = new Patient({
+        firstName: req.params.firstName,
+        lastName: req.params.lastName,
+        gender: req.params.gender,
+        cellPhoneNumber: req.params.cellPhoneNumber,
+        healthInsuranceNumber: req.params.healthInsuranceNumber,
+        address: req.params.address,
+        dateOfBirth: req.params.dateOfBirth,
+        department: req.params.department,
+        doctor: req.params.doctor
       });
-    } else {
-      res.end('Error in first query. ' + err)
-    };
+
+
+      // Create the patient and saving to db
+      newPatient.save(function (error, result) {
+
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+        // Send the patient if no issues
+        res.send(201, result)
+      })
+    })
+
+    //GET
+    // Get all patients in the system
+    server.get('/patients', function (req, res, next) {
+      console.log('GET request: patients');
+      // Find every entity within the given collection
+      Patient.find({}).exec(function (error, result) {
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+        res.send(result);
+      });
+    })
+
+
+    // Get a single patient by their patient id
+    server.get('/patients/:id', function (req, res, next) {
+      console.log("**                                   **");
+      console.log('Processed Request --> GET request: patients/' + req.params.id);
+
+      // Find a single patient by their id
+      Patient.find({ _id: req.params.id }).exec(function (error, patient) {
+        // If there are any errors, pass them to next in the correct format
+        //if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+        if (patient) {
+          // Send the patient if no issues
+          res.send(patient)
+        } else {
+          // Send 404 header if the patient doesn't exist
+          res.send(404)
+        }
+      })
+    })
+
+
+// Get a single patient by their patient healthInsurance_number
+server.get('/patients/hi/:healthInsuranceNumber', function (req, res, next) {
+  console.log("**                                   **");
+  console.log('Processed Request --> GET request: patients/hi/' + req.params.healthInsuranceNumber);
+
+  // Find a single patient by their id
+  Patient.find({healthInsuranceNumber: req.params.healthInsuranceNumber }).exec(function (error, result) {
+    // If there are any errors, pass them to next in the correct format
+    //if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+      res.send(result);
+  })
+
+})
+
+//POST - RECORDS
+// Create a new record
+server.post('/patients/:id/records', function (req, res, next) {
+
+    console.log("**                                   **");
+    console.log("Processed Request --> Create new record");
+  // Make sure fields are defined
+  if (req.params.date === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('date must be supplied'))
+  }
+  if (req.params.doctorId === undefined ) {
+    // If there are any errors, pass them to next in the correct format
+    return next(new restify.InvalidArgumentError('doctorId must be supplied'))
+  }
+
+  var newrecord = new Record ({
+		date: req.params.date,
+		doctorId: req.params.doctorId,
+		tempreture: req.params.tempreture,
+		bloodPressure: req.params.bloodPressure,
+		respiration: req.params.respiration,
+		heartBeat: req.params.heartBeat,
+		bloodOxygen: req.params.bloodOxygen,
+		allergies: req.params.allergies,
+		urine: req.params.urine,
+		nausea: req.params.nausea,
+		appetite: req.params.Appetite,
+		painArea: req.params.PainArea,
+		painLevel: req.params.PainLevel,
+		medications: req.params.Medications,
+		visitReason: req.params.visitReason,
+		healthBackground: req.params.healthBackground,
+		emergencyLevel: req.params.emergencyLevel,
+	});
+
+  // Create the new record using the persistence engine
+  newrecord.save(function (error, result) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send the patient if no issues
+    res.send(201, result)
+    })
+    })
+
+// Get all records in the system of a patient
+  server.get('/patients/:id/records', function (req, res, next) {
+
+    console.log("**                                   **");
+    console.log("Processed Request --> Get all records of patient");
+
+    // Find every entity within the given collection
+    Record.find({}).exec(function (error, result)
+    {
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+      res.send(result);
   });
-}
-
-// Tell the console we're getting ready.
-// The listener in http.createServer should still be active after these messages are emitted.
-console.log('http server will be listening on port %d', theport);
-console.log('CTRL+C to exit');
-
-//
-// House keeping.
-
-//
-// The rudimentary HTML content in three pieces.
-var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
-<head> \
-<style> body {color: #394a5f; font-family: sans-serif} </style> \
-</head> \
-<body> \
-<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
-See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
-<br\> \
-<br\> \
-<br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
-var html2 = '</code></pre> <br\> <i>';
-var html3 = ' documents. </i> <br\> <br\>';
-var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
-var html5 = '</code></pre> <br\> <i>';
-var html6 = ' documents. </i> <br\> <br\> \
-<br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
+})
 
 
+// Get a single record by their patient id
+server.get('/patients/:id/records/:id', function (req, res, next) {
+
+  console.log("**                               **");
+  console.log("Processed Request --> Get by record id");
+  // Find a single patient by their id within save
+  Record.find({ _id: req.params.id }).exec(function (error, result) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+      res.send(result);
+    });
+})
