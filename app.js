b7c7809cdf6a5d983350386227d71269b51370c8
MargoT <mtereshc@gmail.com>
2016-11-14 22:58:10 -0500

    change port back

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ var uristring =
 // The http server will listen to an appropriate port, or default to
 // port 5000
 var ipaddress = process.env.IP; // TODO: figure out which IP to use for the heroku
-var port = process.env.PORT || DEFAULT_PORT;
-//System.getenv("PORT");
+var port = //process.env.PORT || DEFAULT_PORT;
+System.getenv("PORT");
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
